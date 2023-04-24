22c99fed629fd4fd9edede4f0c373d78443b0d82
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-24 15:11:41 +0200

    Trigger the form to submit via the add_task() function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var modals = document.querySelectorAll('.modal');
+    var datepicker = document.querySelectorAll('.datepicker');
+    var selects = document.querySelectorAll('select');
 
     M.Sidenav.init(elems);
     M.Modal.init(modals);
+    M.Datepicker.init(datepicker, {
+        form: 'dd mmm, yyyy',
+        i18n: {
+            done: 'Select'
+        }
+    });
+    M.FormSelect.init(selects);
 });
