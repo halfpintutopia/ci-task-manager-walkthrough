11813f9a1526c8ee0b16579393895dc9d93379fb
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-24 14:41:19 +0200

    Add defensive programming for user to confirm delete, and user authentication.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
+    var modals = document.querySelectorAll('.modal');
+
     M.Sidenav.init(elems);
+    M.Modal.init(modals);
 });
